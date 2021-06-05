03e865878c8c9fe44fee35c0a528834cca4ed9f7
Kacper Bąk <kacper.bak@sonderfin.com>
2021-06-05 13:19:43 +0200

    syncing

--- main_test.go
+++ main_test.go
@@ -6,20 +6,35 @@ import (
 
 func TestQuadraticEquationIfBelowZero(t *testing.T) {
 	QuadraticEquation(1, 2, 3)
+	testCases := []struct {
+		desc string
+	}{
+		{
+			desc: "Testing if Quadric Equation is below zero",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			//assert := assert.New(t)
+
+			//assert.Equal(t, result, "The two roots should be the same.")
+
+		})
+	}
 }
 
 func TestQuadraticEquationIfGreaterThanZero(t *testing.T) {
 	QuadraticEquation(1, 3, -4)
 }
 
-func TestQuadraticEquationIfZeroA(t *testing.T) {
-	QuadraticEquation(0, 1, 4)
-}
+// func TestQuadraticEquationIfZeroA(t *testing.T) {
+// 	QuadraticEquation(0, 1, 4)
+// }
 
-func TestQuadraticEquationIfZeroB(t *testing.T) {
-	QuadraticEquation(1, 0, 4)
-}
+// func TestQuadraticEquationIfZeroB(t *testing.T) {
+// 	QuadraticEquation(1, 0, 4)
+// }
 
-func TestQuadraticEquationIfZeroC(t *testing.T) {
-	QuadraticEquation(1, 3, 0)
-}
+// func TestQuadraticEquationIfZeroC(t *testing.T) {
+// 	QuadraticEquation(1, 3, 0)
+// }
